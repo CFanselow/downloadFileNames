f9435287807514ac74c12d2e9ee0084622d5d44a
root <root@ojs04.cedis.fu-berlin.de>
2023-09-01 22:21:50 +0200

    fix file id, locales

--- DownloadFileNamesPlugin.inc.php
+++ DownloadFileNamesPlugin.inc.php
@@ -46,39 +46,38 @@ class DownloadFileNamesPlugin extends GenericPlugin {
 	
 	function downloadHandler($hookName, $params){
 
-		$pathFile =& $params[0];
-		$originalFilename =& $params[1];
-		
-		// get file infos
+		$path =& $params[0];
+		$filename =& $params[1];
 		$contextId = Application::get()->getRequest()->getContext()->getId();
-		
-		$pathSubmissionFile	= Application::get()->getRequest()->getRequestPath();
-		$pathAsArray = explode("/",$pathSubmissionFile);
-		$sizeOfArray = sizeof($pathAsArray);
-		
-		$submissionId = $pathAsArray[$sizeOfArray-3];
-		$galleyId = $pathAsArray[$sizeOfArray-2];
-		$submissionFileId = $pathAsArray[$sizeOfArray-1];
-		
-		$extension = strtolower(pathinfo($pathFile, PATHINFO_EXTENSION));
-		
-		$locale = AppLocale::getLocale(); // ggf. testen als Alternative: $journal->getName($journal->getPrimaryLocale())
-		$acronym = Application::get()->getRequest()->getContext()->getData('acronym',"en_US");		
-	
+		$primaryLocale = Application::get()->getRequest()->getJournal()->getPrimaryLocale();
+		$locale = AppLocale::getLocale(); 
+		$acronym = Application::get()->getRequest()->getContext()->getData('acronym',$locale);
+		if ($acronym==null) {
+			$acronym = Application::get()->getRequest()->getContext()->getData('acronym',$primaryLocale);
+		}
+
+		// get file info
+		$pathArray = explode("/",$path);
+		$submissionId = $pathArray[3];
 		$submissionDao = DAORegistry::getDAO('SubmissionDAO');
 		$submission = $submissionDao->getById($submissionId);
-		
+
+		$extension = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+		$requestPath = Application::get()->getRequest()->getRequestPath();
+		$requestPathArray = explode("/",$requestPath);
+		$fileId = null;
+		if ($extension=="pdf") {$fileId = $requestPathArray[sizeof($requestPathArray)-1];}
+
 		$issueDao = DAORegistry::getDAO('IssueDAO');
 		$issueId = $submission->getCurrentPublication()->getData('issueId');
 		$issue = $issueDao->getById($issueId, $submission->getContextId());
 		$volume = $issue->getVolume();
-		$number = $issue->getNumber();	
+		$number = $issue->getNumber();
 		$pages = $submission->getCurrentPublication()->getData('pages'); // select setting_name, setting_value from publication_settings where setting_name="pages";
-		$fileId = $submissionFileId;
-		
+
 		// switch through types
 		$type = $this->getSetting($contextId, 'type');
-		$filename = "";
+		$newFilename = "";
 		switch ($type) {
 			case 1:
 				$useAcronym = $this->getSetting($contextId, 'acronym');
@@ -86,29 +85,36 @@ class DownloadFileNamesPlugin extends GenericPlugin {
 				$useNumber = $this->getSetting($contextId, 'number');
 				$usePages = $this->getSetting($contextId, 'pages');
 				$useFileId = $this->getSetting($contextId, 'fileId');				
-				if ($useAcronym && isset($acronym)) {$filename .= "_".$acronym ;}
-				if ($useVolume && isset($volume)) {$filename .= "_".$volume ;}
-				if ($useNumber && isset($number)) {$filename .= "_".$number ;}
-				if ($usePages && isset($pages)) {$filename .= "_".$pages ;}
-				if ($useFileId && isset($fileId)) {$filename .= "_".$fileId ;}				
-				if (substr($filename, 0, 1)=="_") {$filename = substr($filename,-(strlen($filename)-1));}
+				if ($useAcronym && isset($acronym)) {$newFilename .= "_".$acronym ;}
+				if ($useVolume && isset($volume)) {$newFilename .= "_".$volume ;}
+				if ($useNumber && isset($number)) {$newFilename .= "_".$number ;}
+				if ($usePages && isset($pages)) {$newFilename .= "_".$pages ;}
+				if ($useFileId && isset($fileId)) {$newFilename .= "_".$fileId ;}				
+				if (substr($newFilename, 0, 1)=="_") {$newFilename = substr($newFilename,-(strlen($newFilename)-1));}
 				break;
 			case 2:
-				if ($originalFilename!=="") {$filename = $originalFilename;}
-				$filename = str_replace(" ","_",$filename);
+				if ($filename!=="") {
+					$newFilename = $filename;
+					$newFilename = str_replace(" ","-",$newFilename);
+				}
 				break;
 			case 3:
-				$filename = strip_tags($submission->getLocalizedTitle());
-				$filename = str_replace(" ","_",$filename);
-				if (strlen($filename)>100) {$filename = substr($filename,0,100)."...";}
+				$authors = $submission->getAuthors();
+				$primaryAuthor = $authors[0]->getFamilyName($locale);
+				if (empty($primaryAuthor)) {
+					$primaryAuthor = $authors[0]->getFamilyName($primaryLocale);
+				}	
+				if (!empty($primaryAuthor)) {$newFilename = $primaryAuthor."-";}
+				$newFilename .= strip_tags($submission->getLocalizedTitle());
+				$newFilename = str_replace(" ","_",$newFilename);
+				if (strlen($newFilename)>100) {$newFilename = substr($newFilename,0,100)."...";}
 				break;
 		}
-		if ($filename=="") {$filename = "document";}
-		$filename .= ".".$extension;
-		$originalFilename = $filename;
+		if ($newFilename=="") {$newFilename = "document";}
+		$newFilename .= ".".$extension;
+		$filename = $newFilename;
 
-		return false;		
-	
+		return false;
 	}
 
 	/**
